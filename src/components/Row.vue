bf088a44fb1481e7465619ced886d74089ca16bd
Don Le <don.le@donske.com.au>
2022-06-28 20:16:08 +1000

    wip

--- src/components/Row.vue
+++ src/components/Row.vue
@@ -16,14 +16,14 @@ const toggle = () => {
     <div @click="toggle" class="cursor-pointer flex justify-between">
       <h1 class="text-xl text-left">{{ service.name }} *</h1>
       <div class="flex space-x-2">
-        <h1 class="italic text-xl md:text-xl">
+        <h1 class="italic text-xl">
           <span class="text-md">from</span> ${{ service.price }}
         </h1>
         <span>+</span>
       </div>
     </div>
     <div v-show="!close">
-      <p class="text-xl md:text-2xl text-left">{{ service.description }}</p>
+      <p class="text-xl text-left">{{ service.description }}</p>
     </div>
   </div>
 </template>
